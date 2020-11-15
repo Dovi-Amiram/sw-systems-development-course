d9f872921cd3b7a97d2f301cecd312ce5a7ae5a2
Dovi-Amiran <dovi.amiram.91@gmail.com>
2020-11-15 20:33:45 +0200

    Adding all my files

--- power.c
+++ power.c
@@ -3,17 +3,21 @@
 #define e (2.71828182846)
 
 double Exponent(int x){
-	return Power(e, x);
+        return Power(e, x);
 }
 
 double Power(double x, int y){
-	double result = 1.0;
-	int i;
-	for (i = y; i > 0; i--){
-		result *= x;
-	}
-	return result;
+        int abs, j;
+        if (y >= 0)
+		abs = y;
+        else abs = -1*y;
+        
+        double result = 1.0;
+       
+        for (j = abs; j > 0; j--){
+            if (y >= 0)
+                result *= x;
+            else result /= x;
+        }
+        return result;
 }
-
-
-
